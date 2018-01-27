53d3cfebd33c38ee047ebf04552065b54304f443
blindegodd <blindegodd@gmail.com>
2018-01-27 12:16:42 +0300

    BotMessenger With Picture

--- AAI_BOT/AAI_BOT/MainBot.xaml.cs
+++ AAI_BOT/AAI_BOT/MainBot.xaml.cs
@@ -17,23 +17,32 @@ namespace AAI_BOT
 
     public partial class MainBot : Window
     {
-
+        //buttons - текст для кнопок
+        //botMess - сообщения бота
         public string[,] buttons = new string[3, 2] { {"Рад помоч!", "Пшел нахуй!" }, {"Ебаш инфу", "Про замая пизди"}, {"none", "none"} };
-        int k = 0;
+        public string[,] botMess = new string[3, 2] { { "Спасибо, что починил меня, Олег!", "" }, { "Вам что-нибудь нужно?", "Долбоеб, что хотел?" }, { "none", "none" } };
+        //k - кол-во лейблов
+        //l - нужно, чтобы сообщение от батонов сдвигалось нормально
+        int k = 0, l = 0;
+        string source = @"F:\projects\AIProjectX\AAI_BOT\AAI_BOT\bin\Debug\stalin.jpg";
 
         public MainBot()
         {
             InitializeComponent();
-            CreateMess(buttons[0, 0], false);
-            CreateMess(buttons[1, 1], true);
+            First.Content = buttons[k, 0];
+            Second.Content = buttons[k, 1];
+            CreateMess(botMess[k, 0], false);
+            k++;
         }
 
+        //Создание сообщения
+        //mess - текст сообщения
+        //im - true - пользователь отправил / false - бот отправил
         public void CreateMess(string mess, bool im)
         {
             Label lab = new Label();
             lab.Content = mess;
             lab.Foreground = Brushes.Black;
-            //lab.FontSize = 26;
 
             if (im)
             {
@@ -48,10 +57,61 @@ namespace AAI_BOT
 
             lab.VerticalAlignment = VerticalAlignment.Top;
 
-            lab.Margin = new Thickness(10, 10 * k + 10, 10, 0);
+            lab.Margin = new Thickness(10, 25 * (k + l) + 10, 10, 0);
             lab.Padding = new Thickness(5, 5, 5, 5);
             la.Children.Add(lab);
-            k++;
         }
+
+        //нормальные ответ
+        private void Answer(object sender, RoutedEventArgs e)
+        {
+            //создание сообщения от пользователя
+            CreateMess(buttons[k - 1, 0], true);
+            //свдиг для ответа бота и создание его сообщения
+            l++;
+            CreateMess(botMess[k, 0], false);
+            //следующие сообщения пользователя buttons
+            First.Content = buttons[k, 0];
+            Second.Content = buttons[k, 1];
+            k++;
+            if (k == 3)
+                ShowImg();
+        }
+
+        //плохие ответы
+        private void AnswerBad(object sender, RoutedEventArgs e)
+        {
+            //создание сообщения от пользователя
+            CreateMess(buttons[k - 1, 1], true);
+            //свдиг для ответа бота и создание его сообщения
+            l++;
+            CreateMess(botMess[k, 1], false);
+            //следующие сообщения пользователя buttons
+            First.Content = buttons[k, 0];
+            Second.Content = buttons[k, 1];
+            k++;
+            if (k == 3)
+                ShowImg();
+        }
+
+        //Показ картиночки
+        public void ShowImg()
+        {
+            Border bord = new Border();
+            bord.Margin = new Thickness(10, 25 * (k + l - 1) + 10, 0, 0);
+            bord.Padding = new Thickness(5, 5, 5, 5);
+            bord.HorizontalAlignment = HorizontalAlignment.Left;
+            bord.VerticalAlignment = VerticalAlignment.Top;
+            //размер / пишешь любую ширину, чтобы найти высоту, то ширину умножаешь на 1,398907103825137 (можешь округлить до целого)
+            //1,398907103825137 - коэф. высчитывается, как оригинальная высота/оригинальная ширина, или наоборот
+            bord.Width = 170; 
+            bord.Height = 237;
+            bord.Background = new SolidColorBrush(Color.FromArgb(255, 138, 161, 177));
+            la.Children.Add(bord);
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(source));
+            bord.Child = img;
+        }
+        
     }
 }
